a1ec5cbefaf02376f1669a7a5393b25de2c9437b
valliplv <valliammai96@outlook.com>
2020-10-28 19:22:35 +0530

    commiting dev files

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'hardsecretkey' #secret key creation
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Valli1996*@localhost/my_flask'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
